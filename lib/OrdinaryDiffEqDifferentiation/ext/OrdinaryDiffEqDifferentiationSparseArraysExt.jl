be3a2f211387b9569b0b9214de903415d18dbfaa
Jürgen Fuhrmann <juergen-fuhrmann@web.de>
2026-07-14 18:24:20 +0200

    Use AbstractSparseMatrixCSC in OrdinaryDiffEqDifferentiationSparseArraysExt

--- lib/OrdinaryDiffEqDifferentiation/ext/OrdinaryDiffEqDifferentiationSparseArraysExt.jl
+++ lib/OrdinaryDiffEqDifferentiation/ext/OrdinaryDiffEqDifferentiationSparseArraysExt.jl
@@ -2,18 +2,18 @@ module OrdinaryDiffEqDifferentiationSparseArraysExt
 
 using OrdinaryDiffEqDifferentiation
 import SparseArrays
-import SparseArrays: nonzeros, spzeros, SparseMatrixCSC, AbstractSparseMatrix
+import SparseArrays: nonzeros, spzeros, SparseMatrixCSC, AbstractSparseMatrix, AbstractSparseMatrixCSC
 
 # Override the sparse checking functions
 OrdinaryDiffEqDifferentiation.is_sparse(::AbstractSparseMatrix) = true
-OrdinaryDiffEqDifferentiation.is_sparse_csc(::SparseMatrixCSC) = true
+OrdinaryDiffEqDifferentiation.is_sparse_csc(::AbstractSparseMatrixCSC) = true
 
 # Override the sparse array manipulation functions
 OrdinaryDiffEqDifferentiation.nonzeros(A::AbstractSparseMatrix) = nonzeros(A)
 OrdinaryDiffEqDifferentiation.spzeros(T::Type, m::Integer, n::Integer) = spzeros(T, m, n)
 
 # Helper functions for accessing sparse matrix internals
-OrdinaryDiffEqDifferentiation.get_nzval(A::SparseMatrixCSC) = A.nzval
-OrdinaryDiffEqDifferentiation.set_all_nzval!(A::SparseMatrixCSC, val) = (A.nzval .= val; A)
+OrdinaryDiffEqDifferentiation.get_nzval(A::AbstractSparseMatrixCSC) = nonzeros(A)
+OrdinaryDiffEqDifferentiation.set_all_nzval!(A::AbstractSparseMatrixCSC, val) = (nonzeros(A) .= val; A)
 
 end
